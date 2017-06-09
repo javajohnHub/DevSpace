ac4b97d92e9fb43a09bf3e4a7d5e62d74d9279cc
javajohnhub <joedw1978@gmail.com>
2017-06-09 19:37:18 -0400

    validate name input

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {ClientAppComponent} from './client.component';
 import {ChatComponent} from './chat/chat.component';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NameComponent } from './name/name.component';
 import {NameService} from "./name/name.service";
 import { RoomComponent } from './room/room.component';
@@ -14,10 +14,11 @@ import { EditorComponent } from './editor/editor.component';
 import {EditorDirective} from "./editor/editor.directive";
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
   declarations: [ClientAppComponent, EditorDirective,ChatComponent, NameComponent, RoomComponent, RoomListComponent, PeopleListComponent, VideoComponent, EditorComponent],
   bootstrap: [ClientAppComponent],
-  imports: [BrowserModule, FormsModule, BsDropdownModule.forRoot(),ModalModule.forRoot()] ,
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, BsDropdownModule.forRoot(),ModalModule.forRoot()] ,
   providers: [NameService, RoomService,],
 })
 
--- client/src/app/name/name.component.ts
+++ client/src/app/name/name.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { NameService } from './name.service';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-name',
   templateUrl: './name.component.html',
   styleUrls: ['./name.component.css']
 })
 export class NameComponent {
-  name: string;
-  constructor(private nameService:NameService) { }
+  name: string = '';
+ submitted: boolean = false;
+  constructor(private nameService:NameService) {
+
+  }
 
   sendName(){
     this.nameService.sendName(this.name);
     this.name = '';
+    this.submitted = true;
   }
 
 }
