762074f86a98b21937381a2906ff2767f8dc14e6
javajohnhub <joedw1978@gmail.com>
2017-06-08 20:15:23 -0400

    server change for peer id

--- chat_server.js
+++ chat_server.js
@@ -24,7 +24,7 @@ app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('index.html', { root: '/dist'});
 });
 server.listen(app.get('port'), function(){
 
